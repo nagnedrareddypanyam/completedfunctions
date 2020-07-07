906be4029321655d0b373884760486e7eec3453d
nagnedrareddypanyam <nanipanyam4262@gmail.com>
2020-07-07 15:11:33 +0530

    underrootfunction

--- underroutefunction.cs
+++ underroutefunction.cs
@@ -1,11 +1,11 @@
- PolylineBuilder loadedpolylinebuilder = null;
+  PolylineBuilder loadedpolylinebuilder = null;
         private void UnderpolylineRoot_Click(object sender, RoutedEventArgs e)//add this new method and in xaml event  for products under button in routeline tree
         {
             try
             {
 
                 var temproutegeom = MyMapView.SketchEditor.Geometry;//for routeline incomplete draw
-                if (temproutegeom != null)
+                if (temproutegeom != null )//check tempgeom  
                 {
                     Esri.ArcGISRuntime.Geometry.Polyline poly = (Esri.ArcGISRuntime.Geometry.Polyline)GeometryEngine.NormalizeCentralMeridian(temproutegeom);
                     if (poly.Parts.Count > 1)
@@ -26,9 +26,38 @@
                     }
 
                 }
+                else  if (importedlinepoints.Count > 1) //check for any import line condition
+                {
+
+                    var pointline = loadrouteline_create(normalizedimportedpoints);
+
+                    var roadPolyline = pointline as Esri.ArcGISRuntime.Geometry.Polyline;
+                    // var roadPolyline = graphic.Geometry as Esri.ArcGISRuntime.Geometry.Polyline;
+                    this.polylineBuilder = new PolylineBuilder(roadPolyline);
+                    if (polylineBuilder.Parts.Count > 1)
+                    {
+                        var item = coordinatesystem_polyline_new(pointline);
+                        foreach (var item1 in item)
+                        {
+                            var ge = Graphiccoordinates_Aftertransform(item1);
+                            var graphic = new Graphic(ge);
+                            Geometry_OnviewTap(graphic);
+                        }
+                    }
+                    else
+                    {
+                        var loadedgraphic = coordinatesystem_polyline(pointline);
+                        var ge = Graphiccoordinates_Aftertransform(loadedgraphic);
+                        var graphic = new Graphic(ge);
+                        Geometry_OnviewTap(graphic);
+                        // route_symbolsadding_webmerc(get);
+
+                    }
+                  
+
+                }
                 else
                 {
-                  
                     DataAccessLayer.RoutRepository objRout = new DataAccessLayer.RoutRepository();
                     DataTable dt = new DataTable();
                     dt = objRout.GetRouteLineDetails(SelectedRoutName);
@@ -47,7 +76,7 @@
                     var loadedpolyline = l1 as Polyline;
                     this.loadedpolylinebuilder = new PolylineBuilder(loadedpolyline);
                     var item2geom = loadrouteline_geom_create_new(pointcollection_load);
-                    
+
                     if (loadedpolylinebuilder.Parts.Count > 1)
                     {
                         var tempgraphic = coordinatesystem_polyline_new(l1);
@@ -61,7 +90,7 @@
                     }
                     else
                     {
-                        var grap=coordinatesystem_polyline(l1);
+                        var grap = coordinatesystem_polyline(l1);
                         Geometry_OnviewTap(grap);
 
                     }
